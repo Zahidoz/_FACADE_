9275e6bf833b29fa326d983512ddb8d0bf9d6d38
Zahid Vahabzade <83206656+Zahidoz@users.noreply.github.com>
2021-10-27 13:00:01 +0400

    Add project files.

---
+++ _FACADE_/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace _FACADE_
+{
+    class Authorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("Chek User"); 
+        }
+    }
+    class Caching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cache");
+        }
+    }
+    class Logging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Log");
+        }
+    }
+    class Validation
+    {
+        public void Validate()
+        {
+            Console.WriteLine("Validate");
+        }
+    }
+
+
+    class CrossCuttingConcerns
+    {
+        public Authorize Authorize { get; set; }
+        public Caching Caching { get; set; }
+        public Logging Logging { get; set; }
+        public Validation Validation { get; set; }
+       
+        public CrossCuttingConcerns(Authorize _authorize, Caching _caching, Logging _logging, Validation _validation)
+        {
+            Authorize = _authorize;
+            Caching = _caching;
+            Logging = _logging;
+            Validation = _validation;
+        }
+        public void UseAll()
+        {
+            Authorize.CheckUser();
+            Logging.Log();
+            Validation.Validate();
+            Caching.Cache();
+            DoSomething();
+        }
+        public void DoSomething()
+        {
+            Console.WriteLine("Do Something");
+        }
+
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            CrossCuttingConcerns cross  = new CrossCuttingConcerns(new Authorize(), new Caching(), new Logging(), new Validation());
+
+            cross.UseAll();
+        }
+    }
+}
